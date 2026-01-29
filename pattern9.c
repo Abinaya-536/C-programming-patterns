5088cf04ddcf77596e01ebe763b4ec8c43d6c03f
Abinaya Kanagaraj <k.abinaya7kanagaraj@gmail.com>
2026-01-29 17:07:54 +0530

    Add files via upload
    solved pattern problems in c

---
+++ pattern9.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main() {
+    for ( int i=0;i<=4;i++) {
+
+        for (int k=1;k<=4-i ; k++) {
+            printf("  ");
+        }
+
+        for (int j=1;j<=2*i-1;j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for ( int i=4;i>=0;i--) {
+        for (int k=1;k<=4-i ; k++) {
+            printf("  ");
+        }
+        for (int j=1;j<=2*i-1;j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
