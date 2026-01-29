6a35245ddd8ebe2fc2c987b18305deb904cd34e8
Abinaya Kanagaraj <k.abinaya7kanagaraj@gmail.com>
2026-01-29 23:08:51 +0530

    Add files via upload
    solved pattern problems in c

---
+++ pattern20.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int n = 5;  
+      for (i = 1; i <= n; i++) {
+
+        // Left stars
+        for (j = 1; j <= i; j++)
+            printf("*");
+
+        // Middle spaces
+        for (j = 1; j <= 2 * (n - i); j++)
+            printf(" ");
+
+               for (j = 1; j <= i; j++)
+            printf("*");
+
+        printf("\n");
+    }
+
+       for (i = n - 1; i >= 1; i--) {
+
+     
+        for (j = 1; j <= i; j++)
+            printf("*");
+
+
+        for (j = 1; j <= 2 * (n - i); j++)
+            printf(" ");
+
+               for (j = 1; j <= i; j++)
+            printf("*");
+
+        printf("\n");
+    }
+
+    return 0;
+}
